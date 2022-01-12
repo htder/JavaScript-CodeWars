f85e722e3a4fb27523cdb6221bbeb9747dfca5d6
Henry Derbyshire <henry@htderbyshire.com>
2022-01-12 15:50:25 +0000

    Completed Two To One Challenge

---
+++ twoToOne.js
@@ -0,0 +1,7 @@
+function longest(s1, s2) {
+  return [...new Set(s1 + s2)].sort().join("");
+}
+
+console.log(longest("aretheyhere", "yestheyarehere"));
+console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding"));
+console.log(longest("inmanylanguages", "theresapairoffunctions"));
