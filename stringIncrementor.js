b9a51a3fe33676c83a3a1e587bd9e8b8be0aa141
Henry Derbyshire <henry@htderbyshire.com>
2022-02-15 19:42:43 +0000

    Removed comments from string incrementor

--- stringIncrementor.js
+++ stringIncrementor.js
@@ -1,18 +1,11 @@
 function incrementString(string) {
   let number = string.replace(/[A-Za-z]/g, "");
-  console.log(string.split(/(\d+)/g))
   let newNumber = "" + (+number + 1);
   for (let i = newNumber.length; i < number.length; i++) {
     newNumber = "0" + newNumber;
   }
   const splitArr = string.split(/(\d+)/)
   return splitArr[0] + newNumber;
-  // const num = string.replace(/[^d.]/g, "");
-  // return num;
-  // const splitArr = string.split(/(\d+)/)
-  // if (splitArr.length === 3) {
-  //   return splitArr[0] + (+splitArr[1]++);
-  // }
 }
 
 console.log(incrementString("foobar000"));
