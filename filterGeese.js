ce2f00ec14f2952b0d7c41542b39e755571e9904
Henry Derbyshire <henry@htderbyshire.com>
2022-02-20 09:55:11 +0000

    Completed Filter Out The Geese Challenge

---
+++ filterGeese.js
@@ -0,0 +1,6 @@
+function gooseFilter (birds) {
+  const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return birds.filter(bird => !geese.includes(bird));
+}
+
+console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]));
